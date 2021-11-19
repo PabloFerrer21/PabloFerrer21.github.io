45cd4834506e2d19fda6e47aa88f8eebe366b095
PabloFerrer21 <pferrer-dalmaua2122@politecnics.barcelona>
2021-11-19 09:13:40 +0100

    First Commit

--- script.js
+++ script.js
@@ -36,16 +36,3 @@ function loadImage(){
     alert("Image is loaded");
 }
 
-function hanFetClick(){
-    var x = document.getElementById("boton");
-    var y = document.getElementById("imagen");
-    if(x){
-        x.addEventListener("click", function(){
-            alert("Boton Pulsado");
-        });
-    }else if(y){
-        y.addEventListener("click", function(){
-            alert("Imagen Pulsada");
-        });
-    }
-}
